f0887ef98445245a1f6250a02f4fe9d31da22d91
Mikhail Lazarev <mikhail@mlabs.city>
2023-09-25 15:44:12 +0300

    fix: voters verification balance filtering - changed: `VerifiedVote` type moved to domain module - changed: `VerifiedVote` balance is number now

--- server/src/services/verify-voters.ts
+++ server/src/services/verify-voters.ts
@@ -1,43 +1,45 @@
 import { ResultAsync } from "neverthrow";
 import { Snapshoter } from "../loaders/services";
-import { LedgerState } from "snapshoter/build/types";
+import { LedgerState, Snapshot } from "snapshoter/build/types";
+import { VerifiedVote, VerifiedVoters, Vote } from "../domain/types";
 
-type VerifiedVoters = {
-  verifiedAt: LedgerState;
-  votes: { voter: string; vote: "yes" | "no"; id: string; balance: string }[];
-};
+export const BALANCE_DECIMALS = 10 ** 18;
 
 export const VerifyVoters =
   (snapshoter: Snapshoter) =>
-  (
-    voteToken: string,
-    votes: { voter: string; vote: "yes" | "no"; id: string }[],
-  ): ResultAsync<VerifiedVoters, { reason: string }> => {
-    const voters = votes.map((v: any) => v.voter);
-    return snapshoter
-      .currentState()
-      .andThen((state) =>
-        snapshoter
-          .snapshotResourceBalancesByAddress(
-            voteToken,
-            state.stateVersion,
-            voters,
-          )
-          .map((balances) => {
-            const verifiedVotes = votes
-              .map((v: { voter: string; vote: "yes" | "no"; id: string }) => ({
-                ...v,
-                balance: ((x) => (x ? x.balance.toString() : ""))(
-                  balances.getBalanceInfo(v.voter),
-                ), // TODO: make this more readable
-              }))
-              .filter((v: any) => v.balance && v.balance.slice(0, -18)); // Remove 18 decimals, 0 will result in emtpy string will get filtered out
+    (
+      voteToken: string,
+      votes: Vote[],
+    ): ResultAsync<VerifiedVoters, { reason: string }> => {
+      const voters = votes.map((v: any) => v.voter);
+      return snapshoter
+        .currentState()
+        .andThen(state =>
+          snapshoter
+            .snapshotResourceBalancesByAddress(
+              voteToken,
+              state.stateVersion,
+              voters,
+            )
+            .map(snapshot => {
+              const verifiedVotes = filterByBalances(votes, snapshot);
+              return {
+                verifiedAt: state,
+                votes: verifiedVotes,
+              };
+            }),
+        )
+        .mapErr((e) => ({ reason: e.message }));
+    };
 
-            return {
-              verifiedAt: state,
-              votes: verifiedVotes,
-            };
-          }),
-      )
-      .mapErr((e) => ({ reason: e.message }));
-  };
+const filterByBalances = (votes: Vote[], snapshot: Snapshot): VerifiedVote[] => {
+  const result: VerifiedVote[] = [];
+  for (const vote of votes) {
+    const balance = snapshot.getBalanceInfo(vote.voter)?.balance;
+    if (!balance) continue;
+    const verifiedBalance = Math.trunc(balance / BALANCE_DECIMALS);
+    if (verifiedBalance <= 0) continue;
+    result.push({ ...vote, balance: verifiedBalance })
+  }
+  return result;
+};
---
+++ server/tests/verify-voters.test.ts
@@ -0,0 +1,83 @@
+import fs from "fs";
+import { PollsJsonRepo } from "../src/repositories/json-repos";
+import voteController from "../src/controllers/vote";
+import { SignedChallenge } from "@radixdlt/radix-dapp-toolkit";
+import { ResultAsync, okAsync } from "neverthrow";
+import { RolaError } from "../src/services/rola/rola";
+import { Poll, Vote } from "../src/domain/types";
+import { Snapshoter } from "../src/loaders/services";
+import { Snapshot } from "snapshoter/build/types";
+import { BALANCE_DECIMALS, VerifyVoters } from "../src/services/verify-voters";
+
+describe('Verify voters tests', () => {
+
+  test('Verify voters', async () => {
+    const verifyVoters = VerifyVoters(MOCK_SNAPSHOTER);
+    const result = await verifyVoters("doesNotMatterMock", VOTES);
+    expect(result.isOk()).toBe(true);
+    const verifiedVotes = result._unsafeUnwrap();
+    expect(verifiedVotes.verifiedAt).toBe(EXPECTED_STATE);
+    expect(verifiedVotes.votes[0].voter).toBe('a0');
+    expect(verifiedVotes.votes[0].balance).toBe(1);
+    expect(verifiedVotes.votes[1].voter).toBe('a3');
+    expect(verifiedVotes.votes[1].balance).toBe(13000);
+  });
+});
+
+const VOTES: Vote[] = [
+  { id: "0", voter: "a0", vote: 'yes' },
+  { id: "1", voter: "a1", vote: 'yes' },
+  { id: "2", voter: "a2", vote: 'no' },
+  { id: "3", voter: "a3", vote: 'no' },
+  { id: "4", voter: "a4", vote: 'yes' },
+  { id: "5", voter: "a5", vote: 'yes' },
+]
+
+const EXPECTED_STATE = { epoch: 11, roundInEpoch: 12, stateVersion: 13 };
+
+const MOCK_SNAPSHOTER: Snapshoter = {
+  currentState: () => okAsync(EXPECTED_STATE),
+  snapshotResourceBalancesByAddress: function (_: string, __: number, ___: string[]): ResultAsync<Snapshot, Error> {
+    return okAsync(MOCK_SNAPSHOT);
+  },
+  snapshotResourceBalances: function (_: string, __: number): ResultAsync<Snapshot, Error> {
+    throw new Error("Function not implemented.");
+  },
+  ownerKeys: function (_: string): ResultAsync<Uint8Array, Error> {
+    throw new Error("Function not implemented.");
+  }
+}
+
+const MOCK_SNAPSHOT: Snapshot = Snapshot.fromBalances(1,
+  [{
+    fromStateVersion: 1,
+    ownerAddress: "a0",
+    tokenAddress: "doesNotMatterMock",
+    balance: 1 * BALANCE_DECIMALS
+  },
+  {
+    fromStateVersion: 1,
+    ownerAddress: "a1",
+    tokenAddress: "doesNotMatterMock",
+    balance: 1
+  },
+  {
+    fromStateVersion: 1,
+    ownerAddress: "a2",
+    tokenAddress: "doesNotMatterMock",
+    balance: 0
+  },
+  {
+    fromStateVersion: 1,
+    ownerAddress: "a3",
+    tokenAddress: "doesNotMatterMock",
+    balance: 13000 * BALANCE_DECIMALS
+  },
+  {
+    fromStateVersion: 1,
+    ownerAddress: "a4",
+    tokenAddress: "doesNotMatterMock",
+    balance: -1
+  },
+  ]
+);
